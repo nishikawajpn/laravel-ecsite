bf70c0701586af1651950943cc46ee4dbb8937a9
nishikawajpn <99392200+nishikawajpn@users.noreply.github.com>
2022-07-30 10:40:26 +0900

    middleware, abort(404)

---
+++ app/Http/Controllers/Owner/ShopController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Shop;
+
+class ShopController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function($request, $next) {
+            $id = $request->route()->parameter('shop');
+            if (!is_null($id)) {
+                $shopsOwnerId = Shop::findOrFail($id)->owner_id;
+                $shopId = (int)$shopsOwnerId;
+                $ownerId = Auth::id();
+                if ($shopId !== $ownerId) {
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
+    }
+
+    public function index()
+    {
+        $ownerId = Auth::id();
+        $shops = Shop::where('owner_id', $ownerId)->get();
+
+        return view('owner.shops.index', compact('shops'));
+    }
+
+    public function edit($id)
+    {
+        $shops = Shop::findOrFail($id);
+        dd($shops);
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+}
