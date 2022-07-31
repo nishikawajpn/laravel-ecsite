e8ac8363623dce0e956907fdad36744b970d0f14
nishikawajpn <99392200+nishikawajpn@users.noreply.github.com>
2022-08-01 05:12:38 +0900

    stock

---
+++ app/Models/Stock.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+use App\Models\Product;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Stock extends Model
+{
+    use HasFactory;
+
+    protected $table = 't_stocks';
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
